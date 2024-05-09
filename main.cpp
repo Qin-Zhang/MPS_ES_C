d95caacd02bd09d4ea5edb5f3d563c32eb113bda
qjr <cerebrumherum@gmail.com>
2024-05-09 13:00:32 +0200

    first commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
-//
-// Created by Morph on 08.05.2024.
-//
+#include <stdio.h>  // Einbinden der Standard Input/Output Bibliothek
+
+
+int main() {
+    // Ausgabe auf der Konsole
+    for (int i = 0; i < 1000; ++i) {
+        printf("Hello, World!\n");
+    }
+    return 0;  // Beendet das Programm mit dem Statuscode 0 (keine Fehler)
+}
